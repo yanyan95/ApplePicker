c909b2cd0f05f50fc2ec0ff931840b6866f14b36
yanyan95 <yl666@scarletmail.rutgers.edu>
2016-09-22 02:10:28 -0400

    slowly getting there...

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class AppleTree : MonoBehaviour {
 
+    //prefab for instantiating apples
+    public GameObject
+        applePrefab;
+    //speed at which the AppleTree moves in meters/second
+    public float
+        speed = 1f;
+    //distance where AppleTree turns around
+    public float
+        leftAndRightEdge = 10f;
+    //chance that AppleTree will change directions
+    public float
+        chanceToChangeDirections = 0.1f;
+    //rate at which apples will be instantiated
+    public float
+        secondsBetweenAppleDrops = 1f;
+
+
 	// Use this for initialization
 	void Start () {
-	
+	//dropping apples every second
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	//basic movement
+    //changing direction
 	}
 }
