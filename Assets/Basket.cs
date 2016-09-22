6e66b2469bba43e0f80743f79c53fb613b8aa29e
yanyan95 <yl666@scarletmail.rutgers.edu>
2016-09-22 19:46:24 -0400

    up till GUI And Game Management section of txtbook..

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -10,6 +10,26 @@ public class Basket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;      //1
+        //the camera's z position sets the how far to push the mouse into 3d
+        mousePos2D.z = -Camera.main.transform.position.z;    //2
+        //convert the point from 2d screen space into 3d game world
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);   //3
+        //move the x position of this basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
 	}
+
+    void OnCollisionEnter (Collision coll)
+    {
+        //find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
+
 }
